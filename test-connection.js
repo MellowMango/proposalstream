83a25ea4460c91ef68d9183de794d95c9f022cbb
Guy Phelps <guymaxphelps@gmail.com>
2024-08-19 22:21:18 -0500

    Initial commit

---
+++ test-connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log('MongoDB Connected...');
+
+    // List databases to verify connection
+    const admin = mongoose.connection.db.admin();
+    const databases = await admin.listDatabases();
+    console.log('Databases:', databases.databases);
+
+    // Close the connection
+    mongoose.connection.close();
+  } catch (err) {
+    console.error('Connection error:', err.message);
+  }
+};
+
+connectDB();
